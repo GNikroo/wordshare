7d666525c2cb3ed5d8f5f18bdc39d7a1c2191cf9
GNikroo <gina.nikroo@gmail.com>
2023-05-02 11:46:16 +0000

    Begin work on current user context

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { createContext, useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import { Route, Switch } from 'react-router-dom';
 import styles from './App.module.css';
@@ -6,39 +8,59 @@ import NavBar from './components/NavBar';
 import SignInForm from './pages/auth/SignInForm';
 import SignUpForm from './pages/auth/SignUpForm';
 
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
+
 function App() {
+    const [currentUser, setCurrentUser] = useState(null);
+    const handleMount = async (event) => {
+        try {
+            const { data } = await axios.get('dj-rest-auth/user/');
+            setCurrentUser(data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    useEffect(() => {
+        handleMount();
+    }, []);
     return (
-        <div className={styles.App}>
-            <NavBar />
-            <Container className={styles.Main}>
-                <Switch>
-                    <Route
-                        exact
-                        path='/'
-                        render={() => <h1>Home page</h1>}
-                    />
-                    <Route
-                        exact
-                        path='/signin'
-                        render={() => <SignInForm />}
-                    />
-                    <Route
-                        exact
-                        path='/signup'
-                        render={() => <SignUpForm />}
-                    />
-                    <Route
-                        render={() => (
-                            <div>
-                                <p>
-                                    The page you're looking for does not exist!
-                                </p>
-                            </div>
-                        )}
-                    />
-                </Switch>
-            </Container>
-        </div>
+        <CurrentUserContext.Provider value={currentUser}>
+            <SetCurrentUserContext.Provider value={setCurrentUser}>
+                <div className={styles.App}>
+                    <NavBar />
+                    <Container className={styles.Main}>
+                        <Switch>
+                            <Route
+                                exact
+                                path='/'
+                                render={() => <h1>Home page</h1>}
+                            />
+                            <Route
+                                exact
+                                path='/signin'
+                                render={() => <SignInForm />}
+                            />
+                            <Route
+                                exact
+                                path='/signup'
+                                render={() => <SignUpForm />}
+                            />
+                            <Route
+                                render={() => (
+                                    <div>
+                                        <p>
+                                            The page you're looking for does not
+                                            exist!
+                                        </p>
+                                    </div>
+                                )}
+                            />
+                        </Switch>
+                    </Container>
+                </div>
+            </SetCurrentUserContext.Provider>
+        </CurrentUserContext.Provider>
     );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,77 @@
+import axios from 'axios';
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/quill.png';
-import { useCurrentUser } from '../contexts/CurrentUserContext';
+import {
+    useCurrentUser,
+    useSetCurrentUser,
+} from '../contexts/CurrentUserContext';
 import styles from '../styles/NavBar.module.css';
+import Avatar from './Avatar';
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
+    const setCurrentUser = useSetCurrentUser();
 
-    const loggedInIcons = <>{currentUser?.username}</>;
+    const handleSignOut = async () => {
+        try {
+            await axios.post('dj-rest-auth/logout/');
+            setCurrentUser(null);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const addPostIcon = (
+        <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to='/posts/create'
+        >
+            <i className='d-sm-block d-md-none p-2 fas fa-plus-square'></i>
+            <span className='d-none d-md-block p-2'>Add post</span>
+        </NavLink>
+    );
+
+    const loggedInIcons = (
+        <>
+            <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to='/newest'
+            >
+                <i className='d-sm-block d-md-none p-2 fas fa-stream'></i>
+                <span className='d-none d-md-block p-2'>Newest</span>
+            </NavLink>
+            <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to='/liked'
+            >
+                <i className='d-sm-block d-md-none p-2 fas fa-heart'></i>
+                <span className='d-none d-md-block p-2'>Liked</span>
+            </NavLink>
+            <NavLink
+                className={styles.NavLink}
+                to='/'
+                onClick={handleSignOut}
+            >
+                <i className='d-sm-block d-md-none p-2 fas fa-sign-out-alt'></i>
+                <span className='d-none d-md-block p-2'>Sign out</span>
+            </NavLink>
+            <NavLink
+                className={styles.NavLink}
+                to={`/profiles/${currentUser?.profile_id}`}
+            >
+                <Avatar
+                    src={currentUser?.profile_image}
+                    text='Profile'
+                    height={40}
+                />
+            </NavLink>
+        </>
+    );
     const loggedOutIcons = (
         <>
             <NavLink
@@ -48,6 +111,7 @@ const NavBar = () => {
                         Share
                     </Navbar.Brand>
                 </NavLink>
+                {currentUser && addPostIcon}
                 <Nav className={styles.Nav}>
                     <NavLink
                         exact

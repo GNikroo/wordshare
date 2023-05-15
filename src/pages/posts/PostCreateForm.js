84b7d2e3b8457812fa543fce21af794c6f4d32de
GNikroo <gina.nikroo@gmail.com>
2023-05-15 11:15:06 +0000

    Remove image upload from PostPage

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,31 +1,24 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { axiosReq } from '../../api/axiosDefaults';
 
-import { Alert, Image } from 'react-bootstrap';
+import { Alert } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
 
 import appStyles from '../../App.module.css';
 import btnStyles from '../../styles/Button.module.css';
 import styles from '../../styles/PostCreateEditForm.module.css';
 
 import { useHistory } from 'react-router-dom';
-import upload from '../../assets/upload.png';
-import Asset from '../../components/Asset';
 
 function PostCreateForm() {
     const [errors, setErrors] = useState({});
     const [postData, setPostData] = useState({
         title: '',
         content: '',
-        image: '',
     });
-    const { title, content, image } = postData;
-
-    const imageInput = useRef(null);
+    const { title, content } = postData;
 
     const history = useHistory();
 
@@ -36,38 +29,20 @@ function PostCreateForm() {
         });
     };
 
-    const handleChangeImage = (event) => {
-        console.log('handleChangeImage');
-        if (event.target.files.length) {
-            URL.revokeObjectURL(image);
-            setPostData({
-                ...postData,
-                image: URL.createObjectURL(event.target.files[0]),
-            });
-        }
-    };
-
-    const handleCancelImage = () => {
-        URL.revokeObjectURL(image);
-        setPostData({
-            ...postData,
-            image: null,
-        });
-    };
-
     const handleSubmit = async (event) => {
         event.preventDefault();
         const formData = new FormData();
 
         formData.append('title', title);
         formData.append('content', content);
-        if (image) {
-            formData.append('image', imageInput.current.files[0]);
-        }
+
+        console.log(formData, 'form data');
 
         try {
             const { data } = await axiosReq.post('/posts/', formData);
+            console.log(formData, 'form data try catch');
             history.push(`/posts/${data.id}`);
+            console.log(data, 'data');
         } catch (err) {
             console.log(err);
             if (err.response?.status !== 401) {
@@ -77,7 +52,7 @@ function PostCreateForm() {
     };
 
     const textFields = (
-        <div className='text-center'>
+        <div>
             <Form.Group>
                 <Form.Label>Title</Form.Label>
                 <Form.Control
@@ -130,73 +105,14 @@ function PostCreateForm() {
 
     return (
         <Form onSubmit={handleSubmit}>
-            <Row>
-                <Col
-                    className='py-2 p-0 p-md-2'
-                    md={7}
-                    lg={8}
-                >
-                    <Container
-                        className={`${appStyles.Content} ${styles.Container} d-flex`}
-                    >
-                        <Form.Group className='text-center'>
-                            {image ? (
-                                <>
-                                    <figure>
-                                        <Image
-                                            className={appStyles.Image}
-                                            src={image}
-                                            rounded
-                                        />
-                                    </figure>
-                                    <div>
-                                        <Form.Label
-                                            className={`${btnStyles.Button} ${btnStyles.Submit} btn mb-0`}
-                                            htmlFor='image-upload'
-                                        >
-                                            Change the image
-                                        </Form.Label>
-                                        <Button
-                                            onClick={handleCancelImage}
-                                            className={`${btnStyles.Button} ${btnStyles.Cancel}`}
-                                        >
-                                            Cancel upload
-                                        </Button>
-                                    </div>
-                                </>
-                            ) : (
-                                <Form.Label
-                                    className='d-flex justify-content-center'
-                                    htmlFor='image-upload'
-                                >
-                                    <Asset
-                                        src={upload}
-                                        message='Click or tap to upload an image'
-                                    />
-                                </Form.Label>
-                            )}
-                            <Form.File
-                                id='image-upload'
-                                accept='image/*'
-                                onChange={handleChangeImage}
-                                className='invisible'
-                                ref={imageInput}
-                                required={false}
-                            />
-                        </Form.Group>
-                        <div className='d-md-none'>{textFields}</div>
-                    </Container>
-                </Col>
-                <Col
-                    md={5}
-                    lg={4}
-                    className='d-none d-md-block p-0 p-md-2'
-                >
-                    <Container className={appStyles.Content}>
-                        {textFields}
-                    </Container>
-                </Col>
-            </Row>
+            <Container
+                className={`${appStyles.Content} ${styles.Container} d-flex mt-4 align-items-center`}
+            >
+                <div className='d-md-none'>{textFields}</div>
+                <Container className={`${appStyles.Content} d-none d-md-block`}>
+                    {textFields}
+                </Container>
+            </Container>
         </Form>
     );
 }

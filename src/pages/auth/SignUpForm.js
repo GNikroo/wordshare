4b236f6a81e5770428330d0a1b9f8db1189dcc2b
GNikroo <gina.nikroo@gmail.com>
2023-05-24 06:39:08 +0000

    Add about section to the sign up form

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -5,7 +5,15 @@ import btnStyles from '../../styles/Button.module.css';
 import styles from '../../styles/SignInUpForm.module.css';
 
 import axios from 'axios';
-import { Alert, Button, Card, Container, Form } from 'react-bootstrap';
+import {
+    Alert,
+    Button,
+    Card,
+    Col,
+    Container,
+    Form,
+    Row,
+} from 'react-bootstrap';
 import { useRedirect } from '../../hooks/useRedirect';
 
 const SignUpForm = () => {
@@ -41,92 +49,119 @@ const SignUpForm = () => {
     return (
         <Container className={styles.Container}>
             <Card className={`${styles.Card} my-auto p-md-2`}>
-                <Card.Body className={`${styles.Body} p-xs-4`}>
-                    <h1 className={styles.Title}>sign up</h1>
-                    <p className={styles.Text}>and start sharing your ideas!</p>
-                    <Form
-                        className={styles.Form}
-                        onSubmit={handleSubmit}
-                    >
-                        <Form.Group controlId='username'>
-                            <Form.Label className='float-left font-weight-bold'>
-                                Username
-                            </Form.Label>
-                            <Form.Control
-                                type='text'
-                                name='username'
-                                value={username}
-                                onChange={handleChange}
-                            />
-                        </Form.Group>
-                        {errors.username?.map((message, idx) => (
-                            <Alert
-                                variant='warning'
-                                key={idx}
+                <Card.Body className={`${styles.Body}`}>
+                    <Row className='align-items-center'>
+                        <Col>
+                            <h2 className={styles.Title}>sign up</h2>
+                            <p className={styles.Text}>
+                                and start sharing your ideas!
+                            </p>
+                            <Form
+                                className={styles.Form}
+                                onSubmit={handleSubmit}
                             >
-                                {message}
-                            </Alert>
-                        ))}
-                        <Form.Group controlId='password1'>
-                            <Form.Label className='float-left font-weight-bold'>
-                                Password
-                            </Form.Label>
-                            <Form.Control
-                                type='password'
-                                name='password1'
-                                value={password1}
-                                onChange={handleChange}
-                            />
-                        </Form.Group>
-                        {errors.password1?.map((message, idx) => (
-                            <Alert
-                                variant='warning'
-                                key={idx}
+                                <Form.Group controlId='username'>
+                                    <Form.Label className='float-left font-weight-bold'>
+                                        Username
+                                    </Form.Label>
+                                    <Form.Control
+                                        type='text'
+                                        name='username'
+                                        value={username}
+                                        onChange={handleChange}
+                                    />
+                                </Form.Group>
+                                {errors.username?.map((message, idx) => (
+                                    <Alert
+                                        variant='warning'
+                                        key={idx}
+                                    >
+                                        {message}
+                                    </Alert>
+                                ))}
+                                <Form.Group controlId='password1'>
+                                    <Form.Label className='float-left font-weight-bold'>
+                                        Password
+                                    </Form.Label>
+                                    <Form.Control
+                                        type='password'
+                                        name='password1'
+                                        value={password1}
+                                        onChange={handleChange}
+                                    />
+                                </Form.Group>
+                                {errors.password1?.map((message, idx) => (
+                                    <Alert
+                                        variant='warning'
+                                        key={idx}
+                                    >
+                                        {message}
+                                    </Alert>
+                                ))}
+                                <Form.Group controlId='password2'>
+                                    <Form.Label className='float-left font-weight-bold'>
+                                        Confirm password
+                                    </Form.Label>
+                                    <Form.Control
+                                        type='password'
+                                        name='password2'
+                                        value={password2}
+                                        onChange={handleChange}
+                                    />
+                                </Form.Group>
+                                {errors.password2?.map((message, idx) => (
+                                    <Alert
+                                        variant='warning'
+                                        key={idx}
+                                    >
+                                        {message}
+                                    </Alert>
+                                ))}
+                                <Button
+                                    className={`${btnStyles.Button} btn-outline-light`}
+                                    type='submit'
+                                >
+                                    Sign up!
+                                </Button>
+                                {errors.non_field_errors?.map(
+                                    (message, idx) => (
+                                        <Alert
+                                            key={idx}
+                                            variant='warning'
+                                            className='mt-3'
+                                        >
+                                            {message}
+                                        </Alert>
+                                    )
+                                )}
+                            </Form>
+                            <Link
+                                className={styles.Link}
+                                to='/signin'
                             >
-                                {message}
-                            </Alert>
-                        ))}
-                        <Form.Group controlId='password2'>
-                            <Form.Label className='float-left font-weight-bold'>
-                                Confirm password
-                            </Form.Label>
-                            <Form.Control
-                                type='password'
-                                name='password2'
-                                value={password2}
-                                onChange={handleChange}
-                            />
-                        </Form.Group>
-                        {errors.password2?.map((message, idx) => (
-                            <Alert
-                                variant='warning'
-                                key={idx}
-                            >
-                                {message}
-                            </Alert>
-                        ))}
-                        <Button
-                            className={`${btnStyles.Button} btn-outline-light`}
-                            type='submit'
+                                Have an account? <span>Sign in</span>
+                            </Link>
+                        </Col>
+                        <Col
+                            md={5}
+                            className='d-none d-md-block'
                         >
-                            Sign up!
-                        </Button>
-                        {errors.non_field_errors?.map((message, idx) => (
-                            <Alert
-                                key={idx}
-                                variant='warning'
-                                className='mt-3'
-                            >
-                                {message}
-                            </Alert>
-                        ))}
-                    </Form>
-                    <Link
-                        className={styles.Link}
-                        to='/signin'
-                    >
-                        Have an account? <span>Sign in</span>
-                    </Link>
+                            <div className='py-2'>
+                                <h5 className={styles.About}>
+                                    What is Word Share?
+                                </h5>
+                                <p>
+                                    Word Share is a creative space where users
+                                    share ideas, thoughts, and musings or offer
+                                    encouragement and feedback.
+                                    {'\n'}
+                                    <strong>
+                                        If you write it - you can share it.
+                                    </strong>
+                                </p>
+                            </div>
+                        </Col>
+                    </Row>
                 </Card.Body>
             </Card>
         </Container>

909ef15d8c50330cf93bc19431f843c952168b8f
GNikroo <gina.nikroo@gmail.com>
2023-05-27 08:24:21 +0000

    Add height style prompt to asset

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { Spinner } from 'react-bootstrap';
 import styles from '../styles/Asset.module.css';
 
-const Asset = ({ spinner, src, message }) => {
+const Asset = ({ spinner, src, message, height }) => {
     return (
-        <div className={`${styles.Asset} p-4`}>
+        <div className={styles.Asset}>
             {spinner && <Spinner animation='border' />}
             {src && (
                 <img
                     src={src}
                     alt={message}
+                    style={{ height: height }}
                 />
             )}
             {message && <p className='mt-4'>{message}</p>}
